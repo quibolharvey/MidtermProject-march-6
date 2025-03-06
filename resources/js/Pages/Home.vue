94f1a770b1eb9c973c293ba196f51870bd3fe109
quibolharvey <harveyquiboljr>
2025-03-06 22:35:02 +0800

    third commit

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,13 +1,18 @@
 <script setup>
 import App from './App.vue'
 </script>
+
 <template>
-<App>
-  <div class="flex flex-col justify-between mx-4 py-2 md:flex-col text-center">
-    <h1>Welcome to the Midterm Project</h1>
-    <p>Name: Harvey Quibol Jr.</p>
-    <p>Course: BSIT</p>
-    <p>Year: 3rd</p>
-  </div>
+  <App>
+    <div class="flex justify-center items-center min-h-screen bg-gray-100 p-4">
+      <div class="bg-white shadow-lg rounded-2xl p-6 max-w-md w-full text-center border border-gray-200">
+        <h1 class="text-2xl font-bold text-gray-800 mb-2">Welcome to My Midterm Project</h1>
+        <div class="border-t border-gray-300 my-4"></div>
+        <p class="text-lg text-gray-600"><span class="font-semibold">Name:</span> Harvey Quibol Jr.</p>
+        <p class="text-lg text-gray-600"><span class="font-semibold">Course:</span> BSIT</p>
+        <p class="text-lg text-gray-600"><span class="font-semibold">Year:</span> 3rd</p>
+      </div>
+    </div>
   </App>
 </template>
+
